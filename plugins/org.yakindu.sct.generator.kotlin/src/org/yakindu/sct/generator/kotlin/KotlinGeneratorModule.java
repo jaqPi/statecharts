5e85b10015dd8fbf547cbaf0472dbdbaaaa6016e
Andreas Muelder <muelder@itemis.de>
2017-10-12 11:19:21 +0200

    First draft of kotlin code generator

---
+++ plugins/org.yakindu.sct.generator.kotlin/src/org/yakindu/sct/generator/kotlin/KotlinGeneratorModule.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2016 committers of YAKINDU and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of YAKINDU - initial API and implementation
+ */
+package org.yakindu.sct.generator.kotlin;
+
+import static org.yakindu.sct.model.stext.lib.StatechartAnnotations.EVENT_DRIVEN_ANNOTATION;
+
+import org.yakindu.base.types.Annotation;
+import org.yakindu.sct.generator.core.IExecutionFlowGenerator;
+import org.yakindu.sct.generator.core.IGeneratorModule;
+import org.yakindu.sct.generator.core.types.ICodegenTypeSystemAccess;
+import org.yakindu.sct.generator.kotlin.eventdriven.EventDrivenStatemachine;
+import org.yakindu.sct.generator.kotlin.types.JavaTypeSystemAccess;
+import org.yakindu.sct.model.sexec.naming.INamingService;
+import org.yakindu.sct.model.sgen.GeneratorEntry;
+import org.yakindu.sct.model.sgraph.Statechart;
+
+import com.google.inject.Binder;
+import com.google.inject.Scopes;
+
+/**
+ * 
+ * @author andreas muelder - Initial contribution and API
+ * @author terfloth - extensions for event driven execution
+ * 
+ */
+public class KotlinGeneratorModule implements IGeneratorModule {
+
+	@Override
+	public void configure(GeneratorEntry entry, Binder binder) {
+
+		configureGeneratorRoot(entry, binder);
+		configureForExecutionStyle(entry, binder);
+		configureServices(entry, binder);
+	}
+
+	public void configureGeneratorRoot(GeneratorEntry entry, Binder binder) {
+		binder.bind(IExecutionFlowGenerator.class).to(JavaGenerator.class);
+	}
+
+	public void configureForExecutionStyle(GeneratorEntry entry, Binder binder) {
+		Statechart statechart = (Statechart) entry.getElementRef();
+		Annotation eventDrivenAnnotation = statechart.getAnnotationOfType(EVENT_DRIVEN_ANNOTATION);
+
+		if (eventDrivenAnnotation != null) {
+			binder.bind(Statemachine.class).to(EventDrivenStatemachine.class);
+		}
+	}
+
+	public void configureServices(GeneratorEntry entry, Binder binder) {
+
+		final GenmodelEntries entries = new GenmodelEntries();
+		binder.bind(ICodegenTypeSystemAccess.class).to(JavaTypeSystemAccess.class);
+		binder.bind(INamingService.class).to(JavaNamingService.class).in(Scopes.SINGLETON);
+	}
+
+}
